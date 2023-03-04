138b5037ce993e79142b31d11d9b0a8f9b2a928b
Sehrish-Saddique <95633816+Sehrish-Saddique@users.noreply.github.com>
2023-03-05 03:35:20 +0500

    Add files via upload

---
+++ Hackerrank/Day1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string line;
+    getline(cin,line);
+    cout<<"Hello World \n"<<line;
+
+    return 0;
+}
